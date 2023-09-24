a0093e569e190d6d778e49cf7b1bbdb761daf12a
saec08531 <saec08531@gmail.com>
2023-09-25 01:08:26 +0530

    using model mapper

--- Virat/spring-boot-rest-api/src/main/java/com/example/exception/StudentNotFoundException.java
+++ Virat/spring-boot-rest-api/src/main/java/com/example/exception/StudentNotFoundException.java
@@ -2,9 +2,6 @@ package com.example.exception;
 
 public class StudentNotFoundException extends RuntimeException {
 
-    public StudentNotFoundException() {
-    }
-
     public StudentNotFoundException(String message) {
         super(message);
     }
