561485eb283501aa89d317987983651a341577b5
sarath2020 <sarathbabu.nasika9@gmail.com>
2023-12-07 00:41:04 +0530

    Sample java examples

---
+++ Virat/spring-boot-rest-api/src/main/java/com/example/test/TestDates.java
@@ -0,0 +1,10 @@
+package com.example.test;
+
+import java.time.LocalDate;
+
+public class TestDates {
+
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+    }
+}
