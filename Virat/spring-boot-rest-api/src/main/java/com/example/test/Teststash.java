35544e0388b5867e8096b2dab93d0fb2b1790cec
saec08531 <saec08531@gmail.com>
2023-12-31 21:31:09 +0530

    using LocalDate

--- Virat/spring-boot-rest-api/src/main/java/com/example/test/Teststash.java
+++ Virat/spring-boot-rest-api/src/main/java/com/example/test/Teststash.java
@@ -1,8 +1,12 @@
 package com.example.test;
 
+import java.time.LocalDate;
+
 public class Teststash {
 
     public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
 
     }
 }
